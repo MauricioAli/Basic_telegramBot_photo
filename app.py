98a605233bd4fa0f21a2fc5587c7ecd0f4791331
Mauricio Alí <50555673+MauricioAli@users.noreply.github.com>
2021-01-21 20:54:22 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,54 @@
+import logging
+
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+
+# Enable logging
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
+
+logger = logging.getLogger(__name__)
+
+
+
+def start(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('Hi!')
+
+
+def help_command(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Help!')
+
+
+def echo(update: Update, context: CallbackContext) -> None:
+    """Echo the user message."""
+   # file=context.bot.get_file(update.message.document).download("a.png")
+    update.message.reply_text(update.message.text)
+#las imagenes tiene que estar alojadas en un servidor
+def images(update,context):
+    context.bot.send_photo(chat_id=update.effective_chat.id, photo = 'https://1.bp.blogspot.com/-q79uC26Cous/XovFGGoMUoI/AAAAAAAAWEs/aVpq-5qhd5AXvYxn5vkvJsZRXWR46M4PQCLcBGAsYHQ/s1600/LASC_Valores%2BScrum_Gazafatonario.png')
+
+def main():
+    """Start the bot."""
+    # Create the Updater and pass it your bot's token.
+    # Make sure to set use_context=True to use the new context based callbacks
+    # Post version 12 this will no longer be necessary
+    updater = Updater("Token", use_context=True)
+
+    # Get the dispatcher to register handlers
+    dispatcher = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("help", help_command))
+    dispatcher.add_handler(CommandHandler("imagen",images))
+    # Start the Bot
+    updater.start_polling()
+
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
